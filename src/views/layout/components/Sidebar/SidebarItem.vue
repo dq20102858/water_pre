67b34956650b82945564350a0b1caa33eff04638
heqingtong@foxmail.com <heqingtong@foxmail.com>
2020-07-14 13:53:57 +0800

    1

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -12,8 +12,11 @@
           :id="'menu_'+item.children[0].path"
           :class="{'is_active':isActive}"
         >
-          <em>
+          <em v-if="item.children[0].path !=''">
             <img :src="require('@/assets/image/m_'+item.children[0].path+'.png')" />
+          </em>
+           <em v-else>
+            <img :src="require('@/assets/image/m_detault.png')" />
           </em>
           <span
             v-if="item.children[0].meta&&item.children[0].meta.title"
@@ -22,32 +25,8 @@
         </el-menu-item>
       </router-link>
     </template>
-
-    <!-- <template v-for="item in routes" v-if="!item.hidden&&item.children">
-			<router-link  v-if="item.redirect=='dashboard'" :to="item.path+'/'+item.children[0].path" :key="item.children[0].name">
-				<el-menu-item  :index="item.path+'/'+item.children[0].path" :class="{'submenu-title-noDropdown':!isNest}">
-					<em><img :src="require('@/assets/image/m_'+item.children[0].path+'.png')" /></em>
-					<span v-if="item.children[0].meta&&item.children[0].meta.title" slot="title">{{item.children[0].meta.title}}</span>
-				</el-menu-item>
-			</router-link>
-			<el-submenu v-else :index="item.name||item.path" :key="item.name">
-				<template slot="title">
-					<i class="iconfont" :class="item.meta.icon"></i>
-					<span v-if="item.meta&&item.meta.title" slot="title">{{item.meta.title}} {{item.id}}</span>
-				</template>
-				<template v-for="child in item.children" v-if="!child.hidden">
-					<sidebar-item :is-nest="true" class="nest-menu" v-if="child.children&&child.children.length>0" :routes="[child]" :key="child.path"></sidebar-item>
-					<router-link v-else :to="item.path+'/'+child.path" :key="child.name">
-						<el-menu-item :index="item.path+'/'+child.path">
-							<span v-if="child.meta&&child.meta.title" slot="title">{{child.meta.title}}</span>
-						</el-menu-item>
-					</router-link>
-				</template>
-			</el-submenu>
-    </template>-->
   </div>
 </template>
-
 <script>
 export default {
   name: "SidebarItem",
